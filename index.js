1073fa6aa3a9b6b949445b8cc14bebcdae8d62a1
Aastha Nayak <aastha.nayak2002@gmail.com>
2022-08-18 05:23:10 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("side-nav-bar").style.width = "325px";
+}
+function closeNav() {
+    document.getElementById("side-nav-bar").style.width = "0";
+  }
