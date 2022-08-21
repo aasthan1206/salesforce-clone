b31aa6551440e147a182f9f03bf898bc72b909f7
aasthan1206 <aastha.nayak2002@gmail.com>
2022-08-21 17:08:32 +0530

    final commit with functionality

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ function openNav() {
 }
 function closeNav() {
     document.getElementById("side-nav-bar").style.width = "0";
-  }
+}
+function openProducts() {
+    document.getElementById("side-nav-bar-products").style.width = "325px";
+}
+function closeNav() {
+    document.getElementById("side-nav-bar-products").style.width = "0";
+    document.getElementById("side-nav-bar").style.width = "0";
+}
